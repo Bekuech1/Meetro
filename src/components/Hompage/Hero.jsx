92947f5e20bed6c55899593a9fcf5f25859c6fa1
bekuech1 <igboanugobekuechi22@yahoo.com>
2025-03-19 18:36:47 +0000

    fixing hero section

--- src/components/Hompage/Hero.jsx
+++ src/components/Hompage/Hero.jsx
@@ -7,14 +7,17 @@ const Hero = () => {
 
         <Navbar />
 
-        <div className="h-fit grid justify-center items-center w-[91.4%] md:w-[687px] mx-auto mt-[49vh] gap-10">
-          <div className="md:hidden block bg-transparent small text-center paytone py-2"> 
-            <span className="font-extrabold pr-2">Meetro App</span>
-            <button className=" bg-linear-to-tr from-[#AFFC41] to-lime-900 rounded-lg py-0.5 px-3 font-semibold text-white ">Coming Soon</button> 
-          </div>
-          <div>
-            <h1 className="capitalize text-[60px] font-[400] text-white text-center leading-[100%] md:leading-14">Never Miss an <span className="text-[#AFFC41] paytone">Event</span> Around You Again</h1>
-          </div>
+        <div className="h-fit grid justify-center items-center w-[91.4%] md:w-[687px] mx-auto mt-[49vh] md:mt-[40vh] gap-10"> 
+           <div className="grid gap-[10px]">
+                <div className="bg-white/10 backdrop-blur-[24px] flex paytone items-center justify-center gap-[10px] md:hidden fix px-3 py-[6px] w-fit mx-auto"> 
+                    <img src='mobile.svg' alt="phone" className="inline-block" />
+                    <p className="font-[400] text-[12px] text-white">Meetro App</p>
+                    <button className=" bg-linear-to-tr from-[#97DC37] to-[#055962] rounded-[60px] py-[6px] px-[10px] font-[400] text-white text-[8px] h-fit">Coming Soon</button> 
+                </div> 
+                <div>
+                  <h1 className="capitalize text-[48px] md:text-[60px] font-[400] text-white text-center leading-[48px] md:leading-[100%]">Never Miss an <span className="text-[#AFFC41] paytone">Event</span> Around You Again</h1>
+                </div>
+           </div>
            <div className="hidden md:flex gap-4 w-fit mx-auto">
                 <Button name="Create Event" color="bg-white"/>
                 <Button name="Discover Events" color="bg-[#AFFC41]"/>
