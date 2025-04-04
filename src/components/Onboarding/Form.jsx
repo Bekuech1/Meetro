bff88b48cba9ce21a8eb5225727d4e70d0590e36
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-04 15:17:59 +0100

    added more validation to users input

--- src/components/Onboarding/Form.jsx
+++ src/components/Onboarding/Form.jsx
@@ -3,8 +3,8 @@
 function Form({id, label, type, src, placeholder, value, handleChange, error}) {
        
   return (
-    <div className="mb-2">
-        <label htmlFor={id} className="satoshi font-bold align-middle text-[10px]">{label}</label>
+    <div className="mb-4 w-full">
+        <label htmlFor={id} className="satoshi font-bold align-middle text-[11px]">{label}</label>
         <div className="relative flex gap-4 items-center mt-1">
             <img src={src} alt={`${src}-image`} className="h-5 absolute px-2"/>
             {id === "password" && <img src="eye-slash.png" className="absolute right-6"/> }
@@ -12,7 +12,10 @@ function Form({id, label, type, src, placeholder, value, handleChange, error}) {
             className="border-2 border-slate-300 rounded-lg pl-10 py-1 w-full bg-white"
             value={value} onChange={handleChange}/>
         </div>
-        {error && <span className="text-red-400">{error}</span>}
+        <div className="relative">
+          {error && <span className="text-red-400 text-xs absolute left-0
+           max-w-full">{error}</span>}
+        </div>
     </div>
   )
 }
