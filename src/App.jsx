5a8941a0a8ecc57d7b1f8bbdc9ecab0394e9b4c2
Victor Idam <idamvictorx1@gmail.com>
2025-04-04 01:50:07 +0100

    initialized shad cn and created the layout for the home

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router"
-import Authentication from "./routes/Onboarding/Authentication"
-import Signup from "./routes/Onboarding/Signup"
-import Homepage from "./routes/homepage"
+import { BrowserRouter, Routes, Route } from "react-router";
+import Authentication from "./routes/Onboarding/Authentication";
+import Signup from "./routes/Onboarding/Signup";
+import Homepage from "./routes/homepage";
+import HomepageLayout from "./layouts/HomepageLayout";
+import Home from "./routes/home";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element= {<Homepage />}/>
-      <Route path="/authentication" element={<Authentication />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* Homepage Route */}
+          <Route path="/home" element={<HomepageLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
