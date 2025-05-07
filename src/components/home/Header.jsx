1acd5811b0ac5fcc87b2407940d35f92d22cff02
Chibuikem Igboanugo <chubbynino@gmail.com>
2025-05-07 15:04:05 +0100

    try4

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -60,17 +60,6 @@ const Header = () => {
         </div>
       </section>
 
-      {/* Location Section */}
-      {/* <section className="w-[251px] h-8 flex items-center gap-2 pr-2 pl-1 bg-[#344C3F] rounded-full">
-        <div className="w-fit h-fit rounded-full p-1 bg-[#55695E]">
-          <img src="location-home.svg" alt="Location" />
-        </div>
-        <div className="satoshi text-[14px] font-[500] leading-[20px] text-white uppercase w-full">
-          FCT
-        </div>
-        <img src="arrow-down.svg" alt="" />
-      </section> */}
-
       {/* Actions Section */}
       <section className="flex items-center gap-4">
         {/* Notification */}
@@ -89,13 +78,18 @@ const Header = () => {
             <div className="rounded-full sm:w-6 sm:h-6 w-[18px] h-[18px] flex items-center justify-center bg-[#077D8A] text-white uppercase satoshi text-[8px] sm:text-[10px] font-[700] leading-[18px]">
               NO
             </div>
-            <img src="arrow-down.svg" alt="Arrow Down" className={`w-3 h-3 transition-all duration-300 ease-in-out ${isOpen ? 'rotate-180' : ''}`} />
+            <img
+              src="arrow-down.svg"
+              alt="Arrow Down"
+              className={`w-3 h-3 transition-all duration-300 ease-in-out ${isOpen ? "rotate-180" : ""}`}
+            />
           </button>
 
+          {/* Desktop Dropdown */}
           {isOpen && (
-            <ul className="absolute right-0 mt-3 pb-2 px-4 bg-white rounded-[16px] shadow-lg w-[264px] transform transition-transform duration-300 ease-in-out satoshi z-20 sm:block hidden">
+            <ul className="absolute right-0 mt-3 pb-2 px-4 bg-white rounded-[16px] shadow-lg w-[264px] satoshi z-20 hidden sm:block">
               {/* User Info */}
-              <li className="py-4 grid items-center gap-4 ">
+              <li className="py-4 grid items-center gap-4">
                 <div className="bg-[#077D8A] text-white flex items-center justify-center h-12 w-12 rounded-full uppercase satoshi text-[16px] font-[500]">
                   NO
                 </div>
@@ -117,76 +111,64 @@ const Header = () => {
               {dropdownItems.map((item, index) => (
                 <li
                   key={index}
-                  className={`cursor-pointer flex w-full gap-1 py-2 capitalize satoshi text-[12px] font-[500] leading-[18px] items-center ${ item.className } `}
+                  className={`cursor-pointer flex w-full gap-1 py-2 capitalize satoshi text-[12px] font-[500] leading-[18px] items-center ${item.className}`}
                   onClick={item.onClick}
                 >
                   {item.image && (
-                    <img
-                      src={item.image}
-                      alt={item.text}
-                      className="my-auto w-4 h-4"
-                    />
+                    <img src={item.image} alt={item.text} className="my-auto w-4 h-4" />
                   )}
                   {item.text}
                 </li>
               ))}
             </ul>
           )}
-          
 
-          {/* for mobile */}
+          {/* Mobile Dropdown */}
           {isOpen && (
-            <div  className="fixed inset-0 h-screen flex items-center justify-center z-30 bg-[#00000080]/50 backdrop-blur-[4px] sm:hidden ">
-              <ul
-                className="ml-auto sm:pb-2 pb-0 px-4 bg-white rounded-l-[16px] shadow-lg w-[264px] transform transition-transform duration-300 ease-in-out satoshi z-20 h-screen"
-                >
-            {/* User Info */}
-            <li className="py-4 flex justify-between gap-4 items-start">
-            <div className="grid items-center gap-4">
-                  <div className="bg-[#077D8A] text-white flex items-center justify-center h-12 w-12 rounded-full uppercase satoshi text-[16px] font-[500]">
-                    NO
-                  </div>
-                  <div>
-                    <h4 className="satoshi text-[12px] font-[500] leading-[18px] capitalize">
-                      Newman Ogbo
-                    </h4>
-                    <div className="flex gap-2 text-[12px] satoshi font-[500]">
-                      <span>
-                        2 <span className="text-[#8A9191] text-[10px]">Hosted</span>
-                      </span>
-                      <span>
-                        2 <span className="text-[#8A9191]">Attended</span>
-                      </span>
+            <div className="fixed inset-0 h-screen flex items-center justify-center z-30 bg-[#00000080]/50 backdrop-blur-[4px] sm:hidden">
+              <ul className="ml-auto pb-2 px-4 bg-white rounded-l-[16px] shadow-lg w-[264px] satoshi z-20 h-screen">
+                {/* User Info */}
+                <li className="py-4 flex justify-between gap-4 items-start">
+                  <div className="grid items-center gap-4">
+                    <div className="bg-[#077D8A] text-white flex items-center justify-center h-12 w-12 rounded-full uppercase satoshi text-[16px] font-[500]">
+                      NO
+                    </div>
+                    <div>
+                      <h4 className="satoshi text-[12px] font-[500] leading-[18px] capitalize">
+                        Newman Ogbo
+                      </h4>
+                      <div className="flex gap-2 text-[12px] satoshi font-[500]">
+                        <span>
+                          2 <span className="text-[#8A9191] text-[10px]">Hosted</span>
+                        </span>
+                        <span>
+                          2 <span className="text-[#8A9191]">Attended</span>
+                        </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <img 
-                  src="close-circle.svg" 
-                  alt=""
-                  className="cursor-pointer"
-                  onClick={toggleDropdown} 
-                />
-            </li>
-            {/* Dropdown Items */}
-            {dropdownItems.map((item, index) => (
-              <li
-                key={index}
-                className={`cursor-pointer py-2 flex w-full gap-1 sm:py-2 capitalize satoshi text-[12px] font-[500] leading-[18px] items-center ${item.className}`}
-                onClick={item.onClick}
-              >
-                {item.image && (
                   <img
-                    src={item.image}
-                    alt={item.text}
-                    className="my-auto w-4 h-4"
+                    src="close-circle.svg"
+                    alt="Close Dropdown"
+                    className="cursor-pointer"
+                    onClick={toggleDropdown}
                   />
-                )}
-                {item.text}
-              </li>
-            ))}
-          </ul>
-
-            </div>          
+                </li>
+                {/* Dropdown Items */}
+                {dropdownItems.map((item, index) => (
+                  <li
+                    key={index}
+                    className={`cursor-pointer py-2 flex w-full gap-1 capitalize satoshi text-[12px] font-[500] leading-[18px] items-center ${item.className}`}
+                    onClick={item.onClick}
+                  >
+                    {item.image && (
+                      <img src={item.image} alt={item.text} className="my-auto w-4 h-4" />
+                    )}
+                    {item.text}
+                  </li>
+                ))}
+              </ul>
+            </div>
           )}
         </div>
 
