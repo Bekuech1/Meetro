cd14b306c614a86c4bf2723454e813e435caec80
Chibuikem Igboanugo <chubbynino@gmail.com>
2025-09-08 19:07:10 +0100

    added some minor changes

---
+++ src/hooks/useDisableScroll.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+
+export const useDisableScroll = (disabled) => {
+  useEffect(() => {
+    if (disabled) {
+      // Store the current scroll position
+      const scrollY = window.scrollY;
+      
+      // Disable scrolling
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = '100%';
+      document.body.style.overflow = 'hidden';
+      
+      // Cleanup function to re-enable scrolling
+      return () => {
+        document.body.style.position = '';
+        document.body.style.top = '';
+        document.body.style.width = '';
+        document.body.style.overflow = '';
+        
+        // Restore scroll position
+        window.scrollTo(0, scrollY);
+      };
+    }
+  }, [disabled]);
+};
